bc45106f9e31b9fe57546ac56bdff57dabce4ab3
Daniel Nieto <dnieto@unomaha.edu>
2011-03-01 19:50:16 -0800

    added generic splash

--- src/com/team/project/beatMix.java
+++ src/com/team/project/beatMix.java
@@ -9,5 +9,8 @@ public class beatMix extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        
     }
 }
