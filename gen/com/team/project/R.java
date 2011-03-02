cf4bc06c89cead9a61f52fad484d9aea68a98e66
Daniel Nieto <dnieto@unomaha.edu>
2011-03-01 20:28:35 -0800

    made splash screen clickable

--- gen/com/team/project/R.java
+++ gen/com/team/project/R.java
@@ -15,7 +15,7 @@ public final class R {
         public static final int splashscreen=0x7f020001;
     }
     public static final class id {
-        public static final int imageView1=0x7f050000;
+        public static final int splashscreen=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/team/project/beatMix.java
+++ src/com/team/project/beatMix.java
@@ -2,15 +2,33 @@ package com.team.project;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.view.View;;
 public class beatMix extends Activity {
+	
+	/** Global variable for image button */
+  
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        
-        
+       
+       
+    	//ImageButton splashscreen = (ImageButton) findViewById(R.id.splashscreen);
+    	
     }
+  
+    
+    /**When the splash screen is clicked advance to next screen */
+    public void clickNew(View v)
+    {
+    	this.finish();
+ 
+    	
+    }
+
+    
+    
 }
