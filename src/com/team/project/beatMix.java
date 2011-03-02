427e77a23cd068b74c13dd641d77a76f262b1eee
Daniel Nieto <dnieto@unomaha.edu>
2011-03-01 18:48:12 -0800

    basic android framework

---
+++ src/com/team/project/beatMix.java
@@ -0,0 +1,13 @@
+package com.team.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class beatMix extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
