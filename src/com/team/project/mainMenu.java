977ef56adb2ad48d581e5feb0f29d13e267474e2
Daniel Nieto <dnieto@unomaha.edu>
2011-03-21 11:22:33 -0700

    working audiotrack

--- src/com/team/project/mainMenu.java
+++ src/com/team/project/mainMenu.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -11,11 +12,19 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.widget.Button; 
+import android.view.MotionEvent;
 import android.view.View.OnClickListener;
 import android.view.View;
+import android.view.View.OnTouchListener;
 
 
 public class mainMenu extends Activity implements OnClickListener{
+public byte[] boom;
+int intSize = android.media.AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+        AudioFormat.ENCODING_PCM_16BIT);
+
+AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, intSize, AudioTrack.MODE_STREAM);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,14 @@ public class mainMenu extends Activity implements OnClickListener{
         setContentView(R.layout.splash);
         Button b1 = (Button) findViewById(R.id.playsound);
         
+        try {
+			boom = CreateBuffer("android.resource://" + getPackageName() + "/res/raw/boom");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		playAudioTrack();
+        //b1.setOnTouchListener(this);
         b1.setOnClickListener(this);
   
     
@@ -30,52 +47,101 @@ public class mainMenu extends Activity implements OnClickListener{
 
     
 	public void onClick(View v) {
-
-        try {
-			PlayAudioTrack("android.resource://com.team.project/raw/kick");
-		} catch (IOException e) {
+//android.resource://
+     //   try {
+        	//PlayAudioTrack("android.resource://" + getPackageName() + "/res/raw/boom");
+        	PlayAudioTrack(boom);
+		//} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//	e.printStackTrace();
+		//}
 		
 	}
 
-
-   
-	private void PlayAudioTrack(String filePath) throws IOException
+    private void playAudioTrack()
+    {
+    	at.play();
+    }
+    private void stopAudioTrack()
+    {
+    	at.stop();
+    }
+	
+	private byte[] CreateBuffer(String filePath) throws IOException
 	{
+		if (filePath == null)
+			return null;
+		
+		byte[] byteData = null;
+	    InputStream file = getResources().openRawResource(R.raw.boom);
+	    byteData = new byte[file.available()];
+		   
+	    try {
+	       //in = new FileInputStream( file );
+	      file.read( byteData );
+	        file.close(); 
 
+	    } catch (FileNotFoundException e) {
+	        // TODO Auto-generated catch block
+	    ////    e.printStackTrace();
+	    }
+	    
+	    return byteData;
+	    
+		
+	}
+   
+	private void PlayAudioTrack(byte[] byteData ) 
+	{
+/*
 	    if (filePath==null)
 	        return;
 
 	    byte[] byteData = null;
 
-	    File file = null; 
-	    file = new File(filePath); // sdcard path
-	    byteData = new byte[(int) file.length()];
-	    FileInputStream in = null;
+	     InputStream file = getResources().openRawResource(R.raw.boom);
+	    //file = new File(filePath); // sdcard path
+	    byteData = new byte[file.available()];
+	   
 	    try {
-	        in = new FileInputStream( file );
-	        in.read( byteData );
-	        in.close(); 
+	       //in = new FileInputStream( file );
+	      file.read( byteData );
+	        file.close(); 
 
 	    } catch (FileNotFoundException e) {
 	        // TODO Auto-generated catch block
-	        e.printStackTrace();
+	    ////    e.printStackTrace();
 	    }
-
-	    int intSize = android.media.AudioTrack.getMinBufferSize(8000, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-	            AudioFormat.ENCODING_PCM_8BIT);
-
-	    AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-	                        AudioFormat.ENCODING_PCM_8BIT, intSize, AudioTrack.MODE_STREAM);
-
-	    at.play();
+*/
+	   
+	 //   at.play();
 
 	    at.write(byteData, 0, byteData.length);
-	    at.stop();              
+	  //  at.stop();              
 
 	}
+	
+	
 
+
+	//public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+	//	return false;
+	//}
+
+
+	//public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	//}
+
+	@Override
+	public void onDestroy()
+	{
+		at.stop();
+		super.onDestroy();
+	}
+	
+	
         
 }
