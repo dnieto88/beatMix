b3192cfeb6f1652a93071551327fec704a4681d2
mike <mike@rsdcompany.com>
2011-03-23 20:16:12 -0500

    Removed AudioTrack

--- src/com/team/project/mainMenu.java
+++ src/com/team/project/mainMenu.java
@@ -8,27 +8,12 @@ import java.io.InputStream;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
 import android.widget.Button; 
-import android.view.MotionEvent;
 import android.view.View.OnClickListener;
 import android.view.View;
-import android.view.View.OnTouchListener;
-
 
 public class mainMenu extends Activity implements OnClickListener{
-public byte[] boom;
 private audioMixer mSoundManager;
-int intSize = android.media.AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-        AudioFormat.ENCODING_PCM_16BIT);
-
-AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT, intSize, AudioTrack.MODE_STREAM);
-
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,116 +24,16 @@ AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHA
         mSoundManager = new audioMixer();
         mSoundManager.initSounds(getBaseContext());
         mSoundManager.addSound(1, R.raw.boom);
-        mSoundManager.addSound(2, R.raw.kick);
         
-          
-        try {
-			boom = CreateBuffer("android.resource://" + getPackageName() + "/res/raw/boom");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		//playAudioTrack();
         //b1.setOnTouchListener(this);
         b1.setOnClickListener(this);
-  
-
     
     }
 
-    
 	public void onClick(View v) {
 
-       // mSoundManager.playSound(1);
-        mSoundManager.playSound(2);
-        
-		 
+		mSoundManager.playSound(1);
+        	 
 	}
-
-    private void playAudioTrack()
-    {
-    	at.play();
-    }
-    private void stopAudioTrack()
-    {
-    	at.stop();
-    }
-	
-	private byte[] CreateBuffer(String filePath) throws IOException
-	{
-		if (filePath == null)
-			return null;
-		
-		byte[] byteData = null;
-	    InputStream file = getResources().openRawResource(R.raw.boom);
-	    byteData = new byte[file.available()];
-		   
-	    try {
-	       //in = new FileInputStream( file );
-	      file.read( byteData );
-	        file.close(); 
-
-	    } catch (FileNotFoundException e) {
-	        // TODO Auto-generated catch block
-	    ////    e.printStackTrace();
-	    }
-	    
-	    return byteData;
-	    
-		
-	}
-   
-	private void PlayAudioTrack(byte[] byteData ) 
-	{
-/*
-	    if (filePath==null)
-	        return;
-
-	    byte[] byteData = null;
-
-	     InputStream file = getResources().openRawResource(R.raw.boom);
-	    //file = new File(filePath); // sdcard path
-	    byteData = new byte[file.available()];
-	   
-	    try {
-	       //in = new FileInputStream( file );
-	      file.read( byteData );
-	        file.close(); 
-
-	    } catch (FileNotFoundException e) {
-	        // TODO Auto-generated catch block
-	    ////    e.printStackTrace();
-	    }
-*/
-	   
-	 //   at.play();
-
-	    at.write(byteData, 0, byteData.length);
-	  //  at.stop();              
-
-	}
-	
-	
-
-
-	//public boolean onTouch(View v, MotionEvent event) {
-		// TODO Auto-generated method stub
-	//	return false;
-	//}
-
-
-	//public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
-	//}
-
-	@Override
-	public void onDestroy()
-	{
-		at.stop();
-		super.onDestroy();
-	}
-	
-	
-        
 }
